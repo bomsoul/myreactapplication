8b72cbe3adfd17837cf26299b9baaee3b7f602b3
Teerayuth Artidakorn <bomzakup_123@hotmail.com>
2020-04-13 19:57:30 +0700

    style Expression

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    var myStyle = {
+      fontSize: 100,
+      color: 'ff0000'
+    }
+    return(
+      <div>
+        <h1 style={myStyle}>App Component</h1>
+        <h2>{50*50}</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
